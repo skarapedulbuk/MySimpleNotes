fc6ee006325fae1bf9198f063acc4237cc708b9b
skarapedulbuk <voronezh2@gmail.com>
2021-11-07 23:42:24 +0300

    Добавлен тулбар с кнопкой настроек на фрагмент начального экрана. Чайлд фрагмент скрывается при повторном нажатии на шестеренку. Добавлен тулбар с кнопкой поиска на фрагмент со списком заметок. Добавлен тулбар с кнопкой Поделиться на фрагмент с деталями.

--- app/src/main/java/com/skarapedulbuk/mysimplenotes/ui/MainFragment.java
+++ app/src/main/java/com/skarapedulbuk/mysimplenotes/ui/MainFragment.java
@@ -1,6 +1,9 @@
 package com.skarapedulbuk.mysimplenotes.ui;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -8,8 +11,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentResultListener;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.skarapedulbuk.mysimplenotes.R;
 import com.skarapedulbuk.mysimplenotes.domain.SettingsStorage;
 import com.skarapedulbuk.mysimplenotes.ui.list.ListFragment;
@@ -25,52 +28,122 @@ public class MainFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main_fragment, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            showSettingsFragment();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        settingsStorage = new SettingsStorage(super.requireContext());
-        Bundle settingsBundle = settingsStorage.getSettings();
+        MaterialToolbar toolbar = view.findViewById(R.id.modern_toolbar);
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.action_settings) {
+                showSettingsFragment();
+            }
+            if (item.getItemId() == R.id.action_about) {
+                Toast.makeText(requireContext(), R.string.action_about, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
 
-        isBaseChecked = settingsBundle.getBoolean(SettingsStorage.ARG_BASE_CHECKBOX, false);
-        isAddChecked = settingsBundle.getBoolean(SettingsStorage.ARG_ADDITIONAL_CHECKBOX, false);
+    });
 
-        Button btn_base = view.findViewById(R.id.btn_base_func);
-        Button btn_add = view.findViewById(R.id.btn_add_func);
+        toolbar.setNavigationOnClickListener(v ->
+                Toast.makeText(requireContext(), "Back pressed", Toast.LENGTH_SHORT).show());
+
+    settingsStorage =new
+
+    SettingsStorage(super.requireContext());
+    Bundle settingsBundle = settingsStorage.getSettings();
+
+    isBaseChecked =settingsBundle.getBoolean(SettingsStorage.ARG_BASE_CHECKBOX,false);
+    isAddChecked =settingsBundle.getBoolean(SettingsStorage.ARG_ADDITIONAL_CHECKBOX,false);
+
+    Button btn_base = view.findViewById(R.id.btn_base_func);
+    Button btn_add = view.findViewById(R.id.btn_add_func);
 
         btn_base.setEnabled(isBaseChecked);
         btn_add.setEnabled(isAddChecked);
 
-        getChildFragmentManager().setFragmentResultListener(
-                SettingsFragment.KEY_RESULT,
-                getViewLifecycleOwner(),
-                new FragmentResultListener() {
-                    @Override
-                    public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+    getChildFragmentManager().
 
-                        isBaseChecked = result.getBoolean(SettingsStorage.ARG_BASE_CHECKBOX, false);
-                        isAddChecked = result.getBoolean(SettingsStorage.ARG_ADDITIONAL_CHECKBOX, false);
+    setFragmentResultListener(
+            SettingsFragment.KEY_RESULT,
+            getViewLifecycleOwner(),
+            (requestKey, result) -> {
 
-                        btn_base.setEnabled(isBaseChecked);
-                        btn_add.setEnabled(isAddChecked);
+                isBaseChecked = result.getBoolean(SettingsStorage.ARG_BASE_CHECKBOX, false);
+                isAddChecked = result.getBoolean(SettingsStorage.ARG_ADDITIONAL_CHECKBOX, false);
 
-                    }
-                }
+                btn_base.setEnabled(isBaseChecked);
+                btn_add.setEnabled(isAddChecked);
+
+            }
+    );
+
+        view.findViewById(R.id.btn_settings).
+
+    setOnClickListener(v ->
+
+    showSettingsFragment()
         );
 
-        view.findViewById(R.id.btn_settings).setOnClickListener(v ->
-                getChildFragmentManager().beginTransaction() // здесь, как указано в задании, используем getChildFragmentManager, хотя напрашивается, конечно, диалог
-                        .add(R.id.child_container, new SettingsFragment(), "Settings")
-                        .commit()
-        );
+        view.findViewById(R.id.btn_base_func).
 
-        view.findViewById(R.id.btn_base_func).setOnClickListener(v ->
-                getParentFragmentManager().beginTransaction()
-                        .add(R.id.main_container, new ListFragment())
-                        .addToBackStack("List")
-                        .commitAllowingStateLoss());
+    setOnClickListener(v ->
 
-        view.findViewById(R.id.btn_add_func).setOnClickListener(v ->
-            Toast.makeText(requireContext(), R.string.checkbox_additional, Toast.LENGTH_SHORT).show()
+    getParentFragmentManager().
+
+    beginTransaction()
+                        .
+
+    replace(R.id.main_container, new ListFragment()) // если использовать add, меню фрагмента начального экрана (шестеренка) будет показываться там, где не надо
+            .
+
+    addToBackStack("List")
+                        .
+
+    commit());
+
+        view.findViewById(R.id.btn_add_func).
+
+    setOnClickListener(v ->
+            Toast.makeText(
+
+    requireContext(),R.string.checkbox_additional,Toast.LENGTH_SHORT).
+
+    show()
         );
+}
+
+    public void showSettingsFragment() {
+        if (getChildFragmentManager().findFragmentByTag("Settings") == null) {
+            getChildFragmentManager().beginTransaction()
+                    .add(R.id.child_container, new SettingsFragment(), "Settings")
+                    .commit();
+            Toast.makeText(requireContext(), "Показываю настройки", Toast.LENGTH_SHORT).show();
+        } else {
+            getChildFragmentManager().beginTransaction()
+                    .remove(getChildFragmentManager().findFragmentByTag("Settings"))
+                    .commit();
+            Toast.makeText(requireContext(), "Скрываю настройки", Toast.LENGTH_SHORT).show();
+        }
     }
 }
